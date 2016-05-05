96a3bcbd6b6c933b5b0c698a0c526ca485631fa9
neo_max24 <neo_max24@hotmail.com>
2016-05-05 16:58:15 +0800

    restServer 追加判断传入参数是否为auid

--- restServer/Service1.svc.cs
+++ restServer/Service1.svc.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
-
+using magApiCs;
+using System.Collections;
 namespace restServer
 {
     // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“Service1”。
     // 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 Service1.svc 或 Service1.svc.cs，然后开始调试。
     public class Service1 : IService1
     {
+
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
@@ -44,15 +47,18 @@ namespace restServer
         public List<List<UInt64>> getPairAndResponseIdId(UInt64 id1 = 0, UInt64 id2 = 0, UInt64 auid1 = 0, UInt64 auid2 = 0)
         {
             List<KeyValuePair<string, UInt64>> pair = new List<KeyValuePair<string, ulong>>();
-            //如果某个参数为0，说明没有输入这个参数
-            if (id1 != 0)
-                pair.Add(new KeyValuePair<string, UInt64>("Id", id1));
-            if (id2 != 0)
-                pair.Add(new KeyValuePair<string, UInt64>("Id", id2));
-            if (auid1 != 0)
-                pair.Add(new KeyValuePair<string, UInt64>("AA.AuId", auid1));
-            if (auid2 != 0)
-                pair.Add(new KeyValuePair<string, UInt64>("AA.AuId", auid2));
+            //判断输入参数是id还是AuId
+            if (is_id_auid(id1))
+                pair.Add(new KeyValuePair<string, ulong>("AA.AuId", id1));
+            else
+                pair.Add(new KeyValuePair<string, ulong>("Id", id1));
+            if (is_id_auid(id2))
+                pair.Add(new KeyValuePair<string, ulong>("AA.AuId", id2));
+            else
+                pair.Add(new KeyValuePair<string, ulong>("Id", id2));
+            //
+            //TODO:z在这里调用主程序，返回List<List<UInt64>>
+            //
             //json 返回测试
             List<List<UInt64>> pathList = new List<List<UInt64>>();
             List<UInt64> a = new List<UInt64>() { id1, id2 };
@@ -61,6 +67,63 @@ namespace restServer
             pathList.Add(b);
             return pathList;
             //json返回测试
+        }
+        /// <summary>
+        /// 判断输入id是否为AA.AuId
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private bool is_id_auid(UInt64 id)
+        {
+            magApi mag = new magApi();
+            ArrayList attr = new ArrayList();
+            SortedSet<KeyValuePair<string, UInt64>> nodeList = new SortedSet<KeyValuePair<string, UInt64>>(new SortedSetComparer());
+
+            StringBuilder str = new StringBuilder("composite(AA.AuId=");
+            str.Append(id.ToString());
+            str.Append(')');
+            Dictionary<string, object> dataJson = mag.GetResponse(str: str.ToString(), count: 100000000, attributes: "Id");
+            attr = ((ArrayList)dataJson["histograms"]);
+            foreach (Dictionary<string, object> s in attr)
+            {
+                foreach (Dictionary<string, object> h in (ArrayList)s["histogram"])
+                {
+                    UInt64 idValue;
+                    try
+                    {
+                        idValue = (UInt64)h["value"];
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            idValue = (UInt64)(long)h["value"];
+                        }
+                        catch
+                        {
+                            idValue = (UInt64)(int)h["value"];
+                        }
+                    }
+                    nodeList.Add(new KeyValuePair<string, UInt64>((string)s["attribute"], idValue));
+                }
+            }
+            if (nodeList.Count != 0)
+                return true;
+            else
+                return false;
+        }
+        /// <summary>
+        /// 构造pair的比较函数供set使用
+        /// </summary>
+        private class SortedSetComparer : IComparer<KeyValuePair<string, UInt64>>
+        {
+            public int Compare(KeyValuePair<string, UInt64> x, KeyValuePair<string, UInt64> y)
+            {
+                if (x.Key != y.Key)
+                    return x.Key.CompareTo(y.Key);
+                else
+                    return x.Value.CompareTo(y.Value);
+            }
         }
     }
 }
