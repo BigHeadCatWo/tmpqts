3475e13fa61fba6663d494ba124b83ce58a94d1c
neo_max24 <neo_max24@hotmail.com>
2016-05-12 21:02:13 +0800

    解决起始节点申请or式uri太长的问题

--- algorithm2byWang/algorithm2byWang/Program.cs
+++ algorithm2byWang/algorithm2byWang/Program.cs
@@ -15,26 +15,29 @@ namespace algorithm2byWang
         static void Main(string[] args)
         {
             StreamWriter sw = new StreamWriter(@"ConsoleOutput.txt");
-            Console.SetOut(sw);
+            
             algorithm2byWang test = new algorithm2byWang();
             KeyValuePair<string, UInt64> node1;
             KeyValuePair<string, UInt64> node2;
-            node1 = new KeyValuePair<string, UInt64>("Id", 2016266703);
-            node2 = new KeyValuePair<string, UInt64>("AA.AuId", 1905283345);
+            node1 = new KeyValuePair<string, UInt64>("Id", 2126125555);
+            node2 = new KeyValuePair<string, UInt64>("Id", 2060367530);
             List<List<UInt64>> pathList = test.solve(node1, node2);
-            foreach(var i in pathList)
+            Console.WriteLine("number of path:{0}", pathList.Count);
+            Console.SetOut(sw);
+            lock(pathList)
+            foreach (var i in pathList)
             {
                 Console.WriteLine(string.Join(" - ", i));
             }
             sw.Flush();
-            sw.Close();
-        }
+                sw.Close();
+         }
     }
     public class algorithm2byWang
     {
         public List<List<UInt64>> solve(KeyValuePair<string, UInt64> srcNode, KeyValuePair<string, UInt64> dstNode)
         {
-            SortedSet<List<UInt64>> pathList = new SortedSet<List<ulong>>(new SortedSetComparer());
+            List<List<UInt64>> pathList = new List<List<ulong>>();
             //
             SortedSet<KeyValuePair<string, UInt64>> NextNodeOfSrc = null;
             ArrayList nextNodeAttrOfSrcAuid = null;
@@ -78,6 +81,7 @@ namespace algorithm2byWang
             if (NextNodeOfSrc.Contains(dstNode))
             {
                 //Console.WriteLine("1-hop存在:\n{0}-{1}", srcNode, dstNode);
+                lock(pathList)
                 pathList.Add(new List<ulong>() { srcNode.Value, dstNode.Value });
             }
             else
@@ -101,6 +105,7 @@ namespace algorithm2byWang
                         foreach (var s in instSet)
                         {
                             //Console.WriteLine("{0}-{1}-{2}", srcNode, s, dstNode);
+                            lock(pathList)
                             pathList.Add(new List<ulong>() { srcNode.Value, s.Value, dstNode.Value });
                         }
                     }
@@ -224,84 +229,99 @@ namespace algorithm2byWang
             {
                 if (str1L.Count == 0)
                     return;
-                int maxExprNum = 40;
+                int maxExprNum = 60;
                 List<Task> taskList = new List<Task>();
                 List<List<string>> taskStrList = new List<List<string>>();
                 //ToDo
-                //for(int i=0;i<str1L.Count;i+=maxExprNum)
-                //{
-                //    taskStrList.Add(str1L.GetRange(i,Math.Min(maxExprNum,str1L.Count-i)));
-                //    taskList.Add((str1Lpart) => { });
-                //}
-                UInt64 MaxCount = 1000000;
-                string str1LT = string.Join(",or(", str1L.ToArray());
-                StringBuilder str1 = new StringBuilder($"or({str1LT}");
-                str1.Append(',' + str1L.Last().ToString());
-                for (int i = 0; i < str1L.Count(); i++)
-                    str1.Append(')');
-                magApi mag = new magApi();
-                ArrayList attr = new ArrayList();
-                Dictionary<string, object> dataJson = null;
-                for (int i = 0; i < 10; i++)
+                for(int iPart=0; iPart < str1L.Count; iPart += maxExprNum)
                 {
-                    try
-                    {
-                        dataJson = mag.GetResponse(str: str1.ToString(), count: MaxCount, attributes: "Id,RId,F.FId,C.CId,J.JId,AA.AuId");
-                        break;
-                    }
-                    catch (Exception ex)
-                    {
-                        ;
-                    }
+                    List<string> str1Lpart = str1L.GetRange(iPart, Math.Min(maxExprNum, str1L.Count - iPart));
+                    #region new task
+                    Task taskPart = new Task(()=>
+                        {
+                            UInt64 MaxCount = 1000000;
+                            string str1LT = string.Join(",or(", str1Lpart.ToArray());
+                            StringBuilder str1 = new StringBuilder($"or({str1LT}");
+                            str1.Append(',' + str1Lpart.Last().ToString());
+                            for (int i = 0; i < str1Lpart.Count(); i++)
+                                str1.Append(')');
+                            Console.WriteLine("@@@TestAsync:{0}", str1);
+                            magApi mag = new magApi();
+                            ArrayList attr = new ArrayList();
+                            Dictionary<string, object> dataJson = null;
+                            for (int i = 0; i < 10; i++)
+                            {
+                                try
+                                {
+                                    dataJson = mag.GetResponse(str: str1.ToString(), count: MaxCount, attributes: "Id,RId,F.FId,C.CId,J.JId,AA.AuId");
+                                    break;
+                                }
+                                catch (Exception ex)
+                                {
+                                    ;
+                                }
+                            }
+                            attr = ((ArrayList)dataJson["entities"]);
+                            #region 等待hashOfLastNodeOfDst
+                            hashOfLastNodeOfDst.Wait();
+                            #endregion
+                            #region 查找path
+                            foreach (Dictionary<string, object> s in attr)
+                            {
+                                UInt64 selectedId = Convert.ToUInt64((s["Id"]));
+                                foreach (KeyValuePair<string, object> h in s)
+                                {
+                                    if (h.Key == "logprob" || h.Key == "Id") continue;
+                                    if (h.Key == "RId")
+                                    {
+                                        foreach (object t in (ArrayList)h.Value)
+                                        {
+                                            if (lastNodeOfDstHash.Contains(Convert.ToUInt64(t)))
+                                            {
+                                                lock(pathList)
+                                                pathList.Add(new List<ulong>() { srcNode.Value, selectedId, Convert.ToUInt64(t), dstNode.Value });
+                                                //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, new KeyValuePair<string, UInt64>("Id", selectedId), new KeyValuePair<string, UInt64>("Id", Convert.ToUInt64(t)), dstNode);
+                                            }
+                                        }
+                                        continue;
+                                    }
+                                    if (h.Key == "J" || h.Key == "C")
+                                    {
+                                        foreach (KeyValuePair<string, object> t in (Dictionary<string, object>)h.Value)
+                                        {
+                                            if (lastNodeOfDstHash.Contains(Convert.ToUInt64(t.Value)))
+                                            {
+                                                lock(pathList)
+                                                pathList.Add(new List<ulong>() { srcNode.Value, selectedId, Convert.ToUInt64(t.Value), dstNode.Value });
+                                                //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, new KeyValuePair<string, UInt64>("Id", selectedId), t, dstNode);
+                                            }
+                                        }
+                                        continue;
+                                    }
+                                    foreach (Dictionary<string, object> h1 in (ArrayList)h.Value)
+                                    {
+                                        foreach (KeyValuePair<string, object> t in h1)
+                                        {
+                                            if (lastNodeOfDstHash.Contains(Convert.ToUInt64(t.Value)))
+                                            {
+                                                lock(pathList)
+                                                pathList.Add(new List<ulong>() { srcNode.Value, selectedId, Convert.ToUInt64(t.Value), dstNode.Value });
+                                                //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, new KeyValuePair<string, UInt64>("Id", selectedId), t, dstNode);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        );
+                    #endregion
+                    taskList.Add(taskPart);
                 }
-                attr = ((ArrayList)dataJson["entities"]);
-                #region 等待hashOfLastNodeOfDst
-                hashOfLastNodeOfDst.Wait();
-                #endregion
-                #region 查找path
-                foreach (Dictionary<string, object> s in attr)
+                foreach(var t in taskList)
                 {
-                    UInt64 selectedId = Convert.ToUInt64((s["Id"]));
-                    foreach (KeyValuePair<string, object> h in s)
-                    {
-                        if (h.Key == "logprob" || h.Key == "Id") continue;
-                        if (h.Key == "RId")
-                        {
-                            foreach (object t in (ArrayList)h.Value)
-                            {
-                                if (lastNodeOfDstHash.Contains(Convert.ToUInt64(t)))
-                                {
-                                    pathList.Add(new List<ulong>() { srcNode.Value, selectedId, Convert.ToUInt64(t), dstNode.Value });
-                                    //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, new KeyValuePair<string, UInt64>("Id", selectedId), new KeyValuePair<string, UInt64>("Id", Convert.ToUInt64(t)), dstNode);
-                                }
-                            }
-                            continue;
-                        }
-                        if (h.Key == "J" || h.Key == "C")
-                        {
-                            foreach (KeyValuePair<string, object> t in (Dictionary<string, object>)h.Value)
-                            {
-                                if (lastNodeOfDstHash.Contains(Convert.ToUInt64(t.Value)))
-                                {
-                                    pathList.Add(new List<ulong>() { srcNode.Value, selectedId, Convert.ToUInt64(t.Value), dstNode.Value });
-                                    //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, new KeyValuePair<string, UInt64>("Id", selectedId), t, dstNode);
-                                }
-                            }
-                            continue;
-                        }
-                        foreach (Dictionary<string, object> h1 in (ArrayList)h.Value)
-                        {
-                            foreach (KeyValuePair<string, object> t in h1)
-                            {
-                                if (lastNodeOfDstHash.Contains(Convert.ToUInt64(t.Value)))
-                                {
-                                    pathList.Add(new List<ulong>() { srcNode.Value, selectedId, Convert.ToUInt64(t.Value), dstNode.Value });
-                                    //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, new KeyValuePair<string, UInt64>("Id", selectedId), t, dstNode);
-                                }
-                            }
-                        }
-                    }
+                    t.Start();
                 }
+                Task.WaitAll(taskList.ToArray());
                 #endregion
             });
             #endregion
@@ -321,6 +341,7 @@ namespace algorithm2byWang
                             {
                                 if (nextNodeOfSrcHash.Contains(Convert.ToUInt64(t.Value)))
                                 {
+                                    lock(pathList)
                                     pathList.Add(new List<ulong>() { srcNode.Value, Convert.ToUInt64(t.Value), selectedId, dstNode.Value });
                                     //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, t, new KeyValuePair<string, UInt64>("Id", selectedId), dstNode);
                                 }
@@ -336,6 +357,7 @@ namespace algorithm2byWang
                                     continue;
                                 if (nextNodeOfSrcHash.Contains(Convert.ToUInt64(t.Value)))
                                 {
+                                    lock(pathList)
                                     pathList.Add(new List<ulong>() { srcNode.Value, Convert.ToUInt64(t.Value), selectedId, dstNode.Value });
                                     //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, (t), new KeyValuePair<string, UInt64>("Id", selectedId), dstNode);
                                 }
@@ -361,6 +383,7 @@ namespace algorithm2byWang
                             {
                                 if (lastNodeOfDstHash.Contains(Convert.ToUInt64(t)))
                                 {
+                                    lock(pathList)
                                     pathList.Add(new List<ulong>() { srcNode.Value, selectedId, Convert.ToUInt64(t), dstNode.Value });
                                     //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, new KeyValuePair<string, UInt64>("Id", selectedId), new KeyValuePair<string, UInt64>("Id", Convert.ToUInt64(t)), dstNode);
                                 }
@@ -373,6 +396,7 @@ namespace algorithm2byWang
                             {
                                 if (lastNodeOfDstHash.Contains(Convert.ToUInt64(t.Value)))
                                 {
+                                    lock(pathList)
                                     pathList.Add(new List<ulong>() { srcNode.Value, selectedId, Convert.ToUInt64(t.Value), dstNode.Value });
                                     //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, new KeyValuePair<string, UInt64>("Id", selectedId), t, dstNode);
                                 }
@@ -388,7 +412,7 @@ namespace algorithm2byWang
                                     continue;
                                 if (lastNodeOfDstHash.Contains(Convert.ToUInt64(t.Value)))
                                 {
-
+                                    lock(pathList)
                                     pathList.Add(new List<ulong>() { srcNode.Value, selectedId, Convert.ToUInt64(t.Value), dstNode.Value });
                                     //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, new KeyValuePair<string, UInt64>("Id", selectedId), (t), dstNode);
                                 }
@@ -441,6 +465,7 @@ namespace algorithm2byWang
                                 {
                                     if (nextNodeOfSrcHash.Contains(Convert.ToUInt64(h1["AuId"])) && lastNodeOfDstHash.Contains(Convert.ToUInt64(h1["AfId"])))
                                     {
+                                        lock(pathList)
                                         pathList.Add(new List<ulong>() { srcNode.Value, Convert.ToUInt64(h1["AuId"]), Convert.ToUInt64(h1["AfId"]), dstNode.Value });
                                         ////Console.WriteLine("auidAfId");
                                         //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, new KeyValuePair<string, UInt64>("AuId", Convert.ToUInt64(h1["AuId"])), new KeyValuePair<string, UInt64>("AfId", Convert.ToUInt64(h1["AfId"])), dstNode);
@@ -496,6 +521,7 @@ namespace algorithm2byWang
                                 {
                                     if (nextNodeOfSrcHash.Contains(Convert.ToUInt64(h1["AfId"])) && lastNodeOfDstHash.Contains(Convert.ToUInt64(h1["AuId"])))
                                     {
+                                        lock (pathList)
                                         pathList.Add(new List<ulong>() { srcNode.Value, Convert.ToUInt64(h1["AfId"]), Convert.ToUInt64(h1["AuId"]), dstNode.Value });
                                         ////Console.WriteLine("auidAfId");
                                         //Console.WriteLine("3-hop:{0}-{1}-{2}-{3}", srcNode, new KeyValuePair<string, UInt64>("AfId", Convert.ToUInt64(h1["AfId"])), new KeyValuePair<string, UInt64>("AuId", Convert.ToUInt64(h1["AuId"])), dstNode);
@@ -544,18 +570,29 @@ namespace algorithm2byWang
             }
             threeHop_str2.Wait();
             searchOf2hop.Wait();
-            return pathList.ToList() ;
+            Task.WaitAll();
+            HashSet<List<UInt64>> hashPathList = new HashSet<List<ulong>>(pathList,new SortedSetComparer());
+            return hashPathList.ToList() ;
         }
-        public class SortedSetComparer : IComparer<List<UInt64>>
+        public class SortedSetComparer : IEqualityComparer <List<UInt64>>
         {
-            public int Compare(List<UInt64> x, List<UInt64> y)
+            public bool Equals(List<UInt64> x, List<UInt64> y)
             {
                 if (x.Count != y.Count)
-                    return x.Count.CompareTo(y.Count);
-                for (int i = 0; i < x.Count; i++)
-                    if (x[i] != y[i])
-                        return x[i].CompareTo(y[i]);
-                return x[0].CompareTo(y[0]);
+                    return false;
+                for(int i=0;i<x.Count;i++)
+                {
+                    if (!x[i].Equals(y[i]))
+                        return false;
+                }
+                return true;
+            }
+            public int GetHashCode(List<UInt64> x)
+            {
+                int hash = 19;
+                foreach (var i in x)
+                    hash = hash * 31 + x.GetHashCode();
+                return hash;
             }
         }
     }
